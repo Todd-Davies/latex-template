0daecbd5a35ae80151800ab8e5df95ffc794267c
Todd Davies <todd434@gmail.com>
2014-04-03 20:22:23 +0100

    Updated the build script to allow tex subdirectories

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 aspell -t check content.tex
 aspell -t check meta.tex
+directories=();
+for dir in "${directories[@]%*/}"; do
+  cd $dir;
+  for i in `ls *.tex`; do
+    pdflatex $i;
+  done;
+  cd ..
+done;
 pdflatex notes.tex
 pdflatex kindle.tex
 # In case the Author field isn't set
