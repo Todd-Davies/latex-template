e3299479d536032c7e50567cfbe4a12063283957
Todd Davies <todd434@gmail.com>
2014-12-27 22:28:19 +0000

    Made the build script accept a parameter to compile all of the diagrams too

--- build.sh
+++ build.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+
+compileall=
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -a | --compileall )     compileall=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
 aspell -t check content.tex
 aspell -t check meta.tex
-directories=();
-for dir in "${directories[@]%*/}"; do
-  cd $dir;
-  for i in `ls *.tex`; do
-    pdflatex $i;
+
+if [ "$compileall" = "1" ]; then
+  directories=(diagrams);
+  for dir in "${directories[@]%*/}"; do
+    cd $dir;
+    for i in `ls *.tex`; do
+      pdflatex $i;
+    done;
+    cd ..
   done;
-  cd ..
-done;
+fi
 pdflatex notes.tex
 pdflatex kindle.tex
 # In case the Author field isn't set
